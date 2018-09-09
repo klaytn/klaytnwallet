caea1ca5f2b5deac3231ddcfde002f6122828e54
nujabes403 <nujabes403@gmail.com>
2018-09-09 18:12:59 +0900

    Add WalletCreationStepPlate for creating WalletCreationStep1, 2, 3 ...

---
+++ src/components/WalletCreationStepPlate.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+import Button from 'components/Button'
+
+type Props = {
+
+}
+
+import './WalletCreationStepPlate.scss'
+
+class WalletCreationStepPlate extends Component<Props> {
+  render() {
+    const {
+        stepName,
+        title,
+        description,
+        render,
+        nextStepButtons = [],
+      } = this.props
+    return (
+      <div className="WalletCreationStepPlate">
+        <div className="WalletCreationStepPlate__stepName">{stepName}</div>
+        <header className="WalletCreationStepPlate__title">{title}</header>
+        <p className="WalletCreationStepPlate__description">{description}</p>
+        {render && render()}
+        <div className="WalletCreationStepPlate__nextButtons">
+          {nextStepButtons.map(({ title, onClick, disabled, className }) =>
+            <Button
+              style={{
+                width: `${nextStepButtons.length > 1 && (100 / nextStepButtons.length) - 2}%`,
+              }}
+              className={className}
+              key={title}
+              title={title}
+              onClick={onClick}
+              disabled={disabled}
+            />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default WalletCreationStepPlate
