ef539813d1f2b88aaba1f38137e4e3de55cb6bca
nujabes403 <nujabes403@gmail.com>
2018-09-11 11:11:41 +0900

    Add gasTooltip

--- src/components/TransferForm.js
+++ src/components/TransferForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import cx from 'classnames'
-import { onit } from 'klaytn/onit'
+import ReactTooltip from 'react-tooltip'
 
+import { onit } from 'klaytn/onit'
 import Input from 'components/Input'
 import Button from 'components/Button'
 import EditButton from 'components/EditButton'
@@ -13,7 +14,12 @@ type Props = {
 import './TransferForm.scss'
 
 class TransferForm extends Component<Props> {
+  state = {
+    isEditing: false,
+  }
+
   render() {
+    const { isEditing } = this.state
     const {
       from,
       tokenSymbol = 'KLAY',
@@ -37,23 +43,37 @@ class TransferForm extends Component<Props> {
         <Input name="to" onChange={onChange} className="TransferForm__input" label="To Address" placeholder="Enter the address to send" />
         <Input name="value" onChange={onChange} className="TransferForm__input" label="Amount to Send" placeholder="0.000000" />
         <div className="TransferForm__feeLimit">
+          <ReactTooltip
+            id="gas-tooltip"
+            className="TransferForm__gasTooltip"
+            effect="solid"
+            place="bottom"
+          >
+            Gas Price X Gas Limit
+          </ReactTooltip>
           <p className="TransferForm__feeLimitLabel">
             Transction Fee Limit
-            <span className="TransferForm__questionMark">?</span>
+            <span
+              className="TransferForm__questionMark"
+              data-tip
+              data-for='gas-tooltip'
+            >?</span>
           </p>
           <EditButton className="TransferForm__editButton" />
           <span className="TransferForm__fee">{onit.utils.fromWei(`${totalGasFee}`)} {tokenSymbol}</span>
         </div>
-        <div className="TransferForm__gasInfo">
-          <div className="TransferForm__gasPrice">
-            <span>Gas Price</span>
-            <span>{gasPrice} ston</span>
+        {!isEditing && (
+          <div className="TransferForm__gasInfo">
+            <div className="TransferForm__gasPrice">
+              <span>Gas Price</span>
+              <span>{gasPrice} ston</span>
+            </div>
+            <div className="TransferForm__gasLimit">
+              <span>Gas Limit</span>
+              <span>{totalGasFee / gasPrice}</span>
+            </div>
           </div>
-          <div className="TransferForm__gasLimit">
-            <span>Gas Limit</span>
-            <span>{totalGasFee / gasPrice}</span>
-          </div>
-        </div>
+        )}
         <Button
           title="Send Transaction"
           className="TransferForm__sendTransactionButton"
