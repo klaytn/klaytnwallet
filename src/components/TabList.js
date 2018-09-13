fc8207cb4180320970bd62bf432ab1f0a04ce132
nujabes403 <nujabes403@gmail.com>
2018-09-13 10:54:24 +0900

    Make TabItem selected

--- src/components/TabList.js
+++ src/components/TabList.js
@@ -9,23 +9,31 @@ type Props = {
 }
 
 class TabList extends Component<Props> {
+
   render() {
     const { tabItems } = this.props
+    const [ empty, nextTo ] = (window.location.search || '').split('?next=')
+
     return (
       <div className="TabList">
-        {tabItems.map(({ title, link, icon }) => (
-          <TabItem
-            isActive={
-              link === '/'
-                ? link == window.location.pathname
-                : new RegExp(link).test(window.location.pathname)
-              }
-            icon={icon}
-            key={title}
-            title={title}
-            link={link}
-          />
-        ))}
+        {tabItems.map(({ title, link, icon }) => {
+          return (
+            <TabItem
+              isActive={
+                link === '/'
+                  ? link == window.location.pathname
+                  : nextTo
+                    ? (`/${nextTo}` == link) 
+                    : new RegExp(link).test(window.location.pathname)
+                }
+              icon={icon}
+              key={title}
+              title={title}
+              link={link}
+            />
+          )
+        })
+        })}
       </div>
     )
   }
