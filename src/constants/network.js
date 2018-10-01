afb8d1c4345dea7dca24929cd8fc983bc79bfe52
liam <liam.lim@groundx.xyz>
2018-10-01 17:54:56 +0900

    https 적용

--- src/constants/network.js
+++ src/constants/network.js
@@ -1,3 +1,8 @@
-const APIEntry = `${process.env.API_HOST}${process.env.API_PORT ? `:${process.env.API_PORT}` : ''}`
+
+// is http or https
+const protocol = window ? window.location.protocol : 'http';
+const port = process.env.API_PORT ? `:${process.env.API_PORT}` : '';
+
+const APIEntry = `${protocol}//${process.env.API_HOST}${port}`
 
 export default APIEntry
