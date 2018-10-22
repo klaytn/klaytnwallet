fe251331bbd54a187ba6667bb7a76f0744f38d41
liam <liam.lim@groundx.xyz>
2018-10-22 18:14:08 +0900

    fix pm2 cluster mode config file

--- config/pm2.qa.config.js
+++ config/pm2.qa.config.js
@@ -5,6 +5,14 @@ module.exports = {
     script: './klaytnwalletFront.js',
     args: 'webpack.prod.config.js',
 
+    /**
+     * 실행모드
+     * - 인스턴스 갯수는 1
+     * - 로그파일은 하나로 관리
+     */
+    instances: 1,
+    merge_logs: true,
+
     env: {
       NODE_ENV: 'qa',
     },
