f6aed6b9f37d5655ed464266399ad7ec5f7e1c39
underbleu <underbleu.dev@gmail.com>
2019-03-26 17:28:21 +0900

    change run faucet term

--- src/components/FaucetHowItWork.js
+++ src/components/FaucetHowItWork.js
@@ -12,7 +12,7 @@ const FaucetHowItWork = ({ leftBlock }) => (
       The Test_KLAY Faucet runs on Baobab Network.<br />
       Please run the Test_KLAY Faucet to receive a small amount of Test_KLAY for testing.<br />
       For the purpose of preserving enough Test_KLAY for its community users, Test_KLAYs may not be further distributed if you have recently used the Test_KLAY Faucet.
-      The Test_KLAY Faucet is replenished per every 900 blocks.
+      The Test_KLAY Faucet is replenished per every 86,400 blocks(24 hours).
     </p>
     <div className={cx('FaucetHowItWork__faucetableBlock', {
       'FaucetHowItWork__faucetableBlock--visible': leftBlock,
