4042746899e2452affd82b339ca40d581ef55b28
sparrow.gx <ybt8901@naver.com>
2019-04-19 21:44:22 +0900

    main page design change

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react'
 
 import Modal from 'components/Modal'
 import LandingItem from 'components/LandingItem'
-
+import { Link } from 'react-router'
 import cookie from 'utils/cookie'
 
 import './Landing.scss'
@@ -33,24 +33,35 @@ class Landing extends Component {
           closeModal={this.closeModal}
           isShowingModal={isShowingModal}
         />
-        <div className="Landing">
-          <label className="Landing__label">Baobab network</label>
+        <div className="Landing">     
           <header className="Landing__title">Welcome to Klaytn Wallet</header>
-          <LandingItem
-            title="The Klaytn Network does not store your private key on the server."
-            description="Your private key is stored only in the browser’s storage and is automatically deleted when you close the browser."
-            img="/static/images/illust-not-db.svg"
-          />
-          <LandingItem
-            title="The Klaytn Network recommends that you download the Keystore file and save it securely."
-            description="The Klaytn Wallet shall NOT BE HELD RESPONSIBLE for the loss of your password for the private key or keystore file."
-            img="/static/images/illust-save-key.svg"
-          />
-          <LandingItem
-            title="The Klaytn Network recommends  that you use the Testnet-based Klaytn Wallet for testing purpose ONLY."
-            description="Klaytn Wallet has no obligation to compensate or assume liability for any financial loss arising out of its use for any other purpose."
-            img="/static/images/illust-never-send.svg"
-          />
+          <p className="Landing__label">B a o b a b  N e t w o r k</p>
+          <div className="main__link_list">
+            <ul>
+              <li><Link to={'/create'}><button className="create"><img className="TabItem__icon" src="/static/images/icon-create-on.svg"/>Create New Wallet</button></Link></li>
+              <li><Link to={'/access'}><button className="info"><img className="TabItem__icon" src="/static/images/icon-info-on.svg"/>View Wallet Info</button></Link></li>
+              <li><Link to={'/transfer'}><button className="send"><img className="TabItem__icon" src="/static/images/icon-send-on.svg"/>Send KLAY &amp; Tokens</button></Link></li>
+            </ul>
+          </div>
+          <div className="main__text__box">
+            <div className="main__text__title">About Klaytn Wallet :-)</div>
+            <ul className="main__text__list">
+              <li>
+                The Klaytn Network does not store  your private key on the server.<br />
+                Your private key is stored only in the browser’s storage and is automatically deleted when you close the browser.
+              </li>
+              <li>
+                The Klaytn Network recommends that you download the Keystore file and save it securely.<br />
+                The Klaytn Wallet shall NOT BE HELD RESPONSIBLE for the loss of your password for the private key or keystore file.
+              </li>
+              <li>
+                The Klaytn Network recommends  that you use the Testnet-based Klaytn Wallet for testing purpose ONLY.<br />
+                Klaytn Wallet has no obligation to compensate or assume liability for any financial loss arising out of its use for any other purpose.
+              </li>
+            </ul>
+
+
+          </div>
         </div>
       </Fragment>
     )
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -28,122 +28,124 @@ class Modal extends Component {
       this.setState({ setLanguageName: 'English'})
     }
   }
-
+  mainOpenModel = () => {
+    document.getElementsByClassName('popup__dim')[0].classList.add('open');
+    document.getElementsByClassName('klay__alertPopup')[0].classList.add('open');
+  }
+  mainCloseModel = () => {
+    document.getElementsByClassName('popup__dim')[0].classList.remove('open');
+    document.getElementsByClassName('klay__alertPopup')[0].classList.remove('open');
+  }
   render() {
     const { isCheckedHideForAWeek, setLanguageName } = this.state
     const { closeModal, isShowingModal } = this.props
 
-    return isShowingModal && (
+    return (
       <div className="Modal">
-        <Button 
-          className="languageChange__button"
-          onClick={this.setlLanguageNameChange}
-          title={setLanguageName}
-        />
-        {setLanguageName === 'Korean' ? (
+            <button className="klay__alertPopup__button" onClick={this.mainOpenModel}>
+              Fraud Alert: Fake KLAY on crowdsale (사기 주의 안내 : 클레이튼 사칭 코인 판매 피해 주의)
+              <img className="button__link__icon" src="/static/images/icon-link-off.svg"/>
+            </button>
+        <div className="popup__dim" onClick={this.mainCloseModel}></div>    
+        <div className="klay__alertPopup">
+          
+          <div className="languageChange__button_list">  
+            <Button 
+              className="languageChange__button"
+              className={cx('languageChange__button',{'on': setLanguageName === 'Korean',})}
+                
+              onClick={this.setlLanguageNameChange}
+              title='ENGLISH'
+            />
+            <Button 
+              className="languageChange__button"
+              className={cx('languageChange__button',{'on': setLanguageName === 'English',})}
+              onClick={this.setlLanguageNameChange}
+              title='KOREAN'
+            />
+            <button
+              className="close__button"
+              onClick={this.mainCloseModel}
+            ></button>
+          </div>
+          {setLanguageName === 'Korean' ? (
+              <div className="Modal__inner">
+                <div className="Modal__header">
+                Fraud Alert: Fake KLAY on crowdsale
+                </div>
+                <div className="Modal__message">
+                  <p className="Modal__line__text">
+                  We, Klaytn, have recently noticed that there is an ongoing crowdsale for fake KLAY (Klaytn Token) via fraudulent channels.
+                  </p>
+                  <p className="Modal__line__text">
+                    <b className="Modal__text__bold">We clearly notify that:</b>
+                  </p>
+                  <div className="Model__danger__box">
+                    <p className="Modal__line__text">
+                      1. We are <b className="Modal__text__bold">not making ANY sort of token sales</b> for the public.
+                    </p>
+                    <p className="Modal__line__text">
+                      2. There is <b className="Modal__text__bold">no way for individuals to purchase or invest</b> in KLAY.
+                    </p>
+                    <p className="Modal__line__text">
+                      3. <b className="Modal__text__bold">Any resale of KLAY</b> by anyone is also currently <b className="Modal__text__bold">prohibited.</b>
+                    </p>
+                    <p className="Modal__line__text">
+                      4. KLAY are <b className="Modal__text__bold">not able for any type of sales</b> including resale to individuals. 
+                    </p>
+                    <p className="Modal__line__text">
+                      5. There has <b className="Modal__text__bold">not been any kind of KLAY sales through agencies</b> in the past and in the future as well unless notified by our official website.
+                    </p>
+                  </div>
+                  <p className="Modal__line__text">
+                    Any solicitation of individual investors for the sale of KLAY is an obvious fraud, so please beware of potential financial damage.
+                  </p>
+                  <p className="Modal__line__text">
+                    We are considering to take any possible civil and/or criminal actions against those incurring damage to our value, reputation, and products.
+                  </p>
+                </div>
+                
+              </div>
+          ) : (
             <div className="Modal__inner">
               <div className="Modal__header">
-              Fraud Alert:<br />
-              Fake KLAY on crowdsale
+                사기 주의 안내 : Klaytn 사칭 토큰 판매 피해 주의
               </div>
               <div className="Modal__message">
                 <p className="Modal__line__text">
-                  We, Klaytn, have recently noticed that there is an ongoing crowdsale for fake KLAY (Klaytn Token) via fraudulent channels.
+                  안녕하세요, Klaytn에서 공지드립니다. Klaytn은 최근 Klaytn의 토큰인 KLAY를 사칭하여 코인 투자자를 모집하는 사기 행위를 다수 발견하였습니다.
                 </p>
                 <p className="Modal__line__text">
-                  <b className="Modal__text__bold">We clearly notify that:</b>
+                  <b className="Modal__text__bold">이와 관련하여, 아래와 같이 안내드립니다.</b>
                 </p>
                 <div className="Model__danger__box">
                   <p className="Modal__line__text">
-                    We are <b className="Modal__text__bold">not making ANY sort of token sales</b> for the public.
+                    1. Klaytn은 <b className="Modal__text__bold">대중에게 어떤 종류의 토큰 판매도 하지 않습니다.</b>
                   </p>
                   <p className="Modal__line__text">
-                    There is <b className="Modal__text__bold">no way for individuals to purchase or invest</b> in KLAY.
+                    2. <b className="Modal__text__bold">개인</b>은 KLAY를 어떠한 방법으로도 <b className="Modal__text__bold">구매나 투자할 수 없음</b>을 안내드립니다.
                   </p>
                   <p className="Modal__line__text">
-                    <b className="Modal__text__bold">Any resale of KLAY</b> by anyone is also currently <b className="Modal__text__bold">prohibited.</b>
+                    3. 누구든지<b className="Modal__text__bold">KLAY를 재판매하는 것은 현재 금지</b>되어 있습니다.
                   </p>
                   <p className="Modal__line__text">
-                    KLAY are <b className="Modal__text__bold">not able for any type of sales</b> including resale to individuals. 
+                    4. <b className="Modal__text__bold">KLAY</b>는 개인에 대한 재판매를 포함한 <b className="Modal__text__bold">모든 유형의 판매에 사용할 수 없습니다.</b>
                   </p>
                   <p className="Modal__line__text">
-                    There has <b className="Modal__text__bold">not been any kind of KLAY sales through agencies</b> in the past and in the future as well unless notified by our official website.
+                    5. 공식 홈페이지에서 별도로 통지하지 않는 한, Klaytn은 <b className="Modal__text__bold">대리인 또는 제3자를 통해 KLAY를 판매하지 않으며</b> 앞으로도 판매하지 않을 것 입니다.
                   </p>
                 </div>
                 <p className="Modal__line__text">
-                  Any solicitation of individual investors for the sale of KLAY is an obvious fraud, so please beware of potential financial damage.
+                  KLAY 판매에 대한 개인 투자자 모집 행위는 명백한 사기임을 다시 한 번 강조드리며, 관련 피해가 발생하지 않도록 주의 당부드립니다.
                 </p>
                 <p className="Modal__line__text">
-                  We are considering to take any possible civil and/or criminal actions against those incurring damage to our value, reputation, and products.
+                아울러 Klaytn 및 KLAY를 사칭함으로써 Klaytn의 가치와 명예를 훼손하는 행위에 관련하여서는 엄중하게 민/형사상 법적 조치를 취할 것을 검토 중입니다.
                 </p>
               </div>
-              <div className="Modal__footer">
-                <div
-                  className={cx('Modal__footerCheckBox', {
-                    'Modal__footerCheckBox--checked': isCheckedHideForAWeek,
-                  })}
-                  onClick={this.notShowingForAWeek}
-                />
-                <p className="Modal__footerMessage">Do not show for a week.</p>
-                <div
-                  className="Modal__footerCloseButton"
-                  onClick={closeModal}
-                />
-              </div>
+              
             </div>
-        ) : (
-          <div className="Modal__inner">
-            <div className="Modal__header">
-            사기 주의 안내 :<br />
-            Klaytn 사칭 토큰 판매 피해 주의
-            </div>
-            <div className="Modal__message">
-              <p className="Modal__line__text">
-                안녕하세요, Klaytn에서 공지드립니다. Klaytn은 최근 Klaytn의 토큰인 KLAY를 사칭하여<br />
-                코인 투자자를 모집하는 사기 행위를 다수 발견하였습니다.
-              </p>
-              <p className="Modal__line__text">
-                <b className="Modal__text__bold">이와 관련하여, 아래와 같이 안내드립니다.</b>
-              </p>
-              <div className="Model__danger__box">
-                <p className="Modal__line__text">
-                  Klaytn은 <b className="Modal__text__bold">대중에게 어떤 종류의 토큰 판매도 하지 않습니다.</b>
-                </p>
-                <p className="Modal__line__text">
-                  <b className="Modal__text__bold">개인</b>은 KLAY를 어떠한 방법으로도 <b className="Modal__text__bold">구매나 투자</b>할 수 없음을 안내드립니다.
-                </p>
-                <p className="Modal__line__text">
-                누구든지<b className="Modal__text__bold">KLAY를 재판매하는 것은 현재 금지</b>되어 있습니다.
-                </p>
-                <p className="Modal__line__text">
-                  <b className="Modal__text__bold">KLAY</b>는 개인에 대한 재판매를 포함한 <b className="Modal__text__bold">모든 유형의 판매에 사용할 수 없습니다.</b>
-                </p>
-                <p className="Modal__line__text">
-                공식 홈페이지에서 별도로 통지하지 않는 한, Klaytn은 <b className="Modal__text__bold">대리인 또는 제3자를 통해 KLAY를 판매하지 않으며</b> 앞으로도 판매하지 않을 것 입니다.
-                </p>
-              </div>
-              <p className="Modal__line__text">
-                KLAY 판매에 대한 개인 투자자 모집 행위는 명백한 사기임을 다시 한 번 강조드리며, 관련 피해가 발생하지 않도록 주의 당부드립니다.
-              </p>
-              <p className="Modal__line__text">
-              아울러 Klaytn 및 KLAY를 사칭함으로써 Klaytn의 가치와 명예를 훼손하는 행위에 관련하여서는 엄중하게 민/형사상 법적 조치를 취할 것을 검토 중입니다.
-              </p>
-            </div>
-            <div className="Modal__footer">
-              <div
-                className={cx('Modal__footerCheckBox', {
-                  'Modal__footerCheckBox--checked': isCheckedHideForAWeek,
-                })}
-                onClick={this.notShowingForAWeek}
-              />
-              <p className="Modal__footerMessage">일주일 동안 다시 열지 않음</p>
-              <div
-                className="Modal__footerCloseButton Modal__footerCloseButton_kr"
-                onClick={closeModal}
-              />
-            </div>
-          </div>
-        )}        
+          )}
+        </div>        
       </div>
     )
   }
