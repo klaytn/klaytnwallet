cc1dd5d9bf0db10b53cc060b16b76471dc4da914
YunBongtae <ybt8901@naver.com>
2019-06-14 08:57:55 +0900

    Delete korean disclaimer (#179)
    * package-lock add
    
    * Delete korean disclaimer

--- src/components/WalletCreationReminder.js
+++ src/components/WalletCreationReminder.js
@@ -5,13 +5,11 @@ const WalletCreationReminder = () => (
   <div className="WalletCreationReminder">
     {KLAYTN_URL_NAME == 'Main Network' ? (
         <p>
-          Klaytn Wallet is for development purpose only. Do not use it for personal or commercial use.<br />
-          Klaytn Wallet은 개발 용도로만 사용 가능합니다. 상용 목적의 사용을 금합니다.
+          Klaytn Wallet is for development purpose only. Do not use it for personal or commercial use.
         </p>
       ):(
         <p>
-          Klaytn Wallet is for development purpose only. {KLAYTN_KLAY_UINT} on test has no financial value.<br />
-          Klaytn Wallet은 개발 용도로만 사용 가능합니다. Testnet의 {KLAYTN_KLAY_UINT}는 금전적 가치가 없습니다.  
+          Klaytn Wallet is for development purpose only. {KLAYTN_KLAY_UINT} on test has no financial value.
         </p>
       )
     }
