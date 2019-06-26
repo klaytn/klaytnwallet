7bda18f73ea2362eacd51b9bac834c84bcc8804f
YunBongtae <ybt8901@naver.com>
2019-06-26 19:07:30 +0900

    Remove unnecessary files (#207)
    * package-lock add
    
    * package-lock add
    
    * Remove unnecessary files
    
    * Edit klaytn related files
    
    * package-lock update

--- src/klaytn/caver.js
+++ src/klaytn/caver.js
@@ -1,15 +1,8 @@
 import Caver from 'caver-js'
 
-// const PING_TIME = 3000 // 3s
-
-// const intervalId = setInterval(() =>
-// caverSocket.klay.getBlockNumber().then(console.log), PING_TIME)
-
 const protocol = window ? window.location.protocol : 'http:';
 export const config = {
   rpcURL: `${protocol}//${process.env.KLAYTN_HOST}`,
-  wsURL: 'ws://devnet.klaytn.io:8546', // You can access websocket only with VPN.
 }
 
 export const caver = new Caver(config.rpcURL);
-// export const caverSocket = new caver(new caver.providers.WebsocketProvider(config.wsURL))
